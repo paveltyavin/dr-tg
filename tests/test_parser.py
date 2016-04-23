3d387ffb8f379282a6292174cf897b8089a969c2
Pavel Tyavin <pavel@tyavin.name>
2016-04-24 00:32:32 +0300

    add tests

---
+++ tests/test_parser.py
@@ -0,0 +1,13 @@
+import unittest
+from codecs import open
+
+from parser import Parser
+
+
+class ParserTestCase(unittest.TestCase):
+    def test_get_ko(self):
+        parser = Parser()
+        with open('./pages/6.htm', encoding='cp1251') as f:
+            parser.html = f.read()
+        ko_list = parser.get_ko_list()
+        self.assertEqual([], ko_list)
