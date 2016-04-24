9684a3963d002a029194b202f15a773a71a5fc47
Mysterious <dmitryskalkin@gmail.com>
2016-04-24 12:23:52 +0300

    переписал тест, чтобы он проверял создается ли словарь вида {'название списка ко': ['1', '1+']}

--- d_parser.py
+++ d_parser.py
@@ -1,9 +1,10 @@
 from grab.base import Grab
-
+import re
 
 class Parser(object):
     def __init__(self):
         self.g = Grab()
+        self.red_span_re = re.compile(r'<span style="color:red">([123]\+?|N)</span>')
 
     def get_html(self):
         """
@@ -14,20 +15,24 @@ class Parser(object):
 
     def get_ko_list(self):
         """
-        Возвращает список КО по секторам. Каждый сектор представляет собой словарь.
+        Возвращает списки КО, в виде словаря, в котором ключ - название группы КО, значение - список КО.
         Пример:
-        [{
-            'name': 'Первый сектор',
-            'ko': ['1+', '1', '3'],
-        },{
-            'name': 'Второй сектор',
-            'ko' = ['1', '2'],
-        }]
-
-        В обычном уровне (без секторов)
-        [{
-            'name': '',
-            'ko': ['1+', '1', '3'],
-        }]
+        {'основные коды': ['1+', '1', '3'], 'бонусные коды': ['2', '3', '1']}
         """
-        return []
+        div_list = self.g.doc.select('//div[@class="zad"]')
+        for i in div_list:
+            if '<strong>Коды сложности</strong>' in i.html():
+                div_html = i.html()
+        ko_part = div_html.split('<strong>Коды сложности</strong><br> ')[1]
+        ko_part = ko_part.replace('null', 'N').replace('<br></div>', '').replace('\n', '').replace('\r', '')
+        ko_part = self.red_span_re.sub('V', ko_part)
+        code_lists = ko_part.split('<br>')
+        code_dict = {}
+        for i in code_lists:
+            ko_dict = i.split(': ')
+            key = ko_dict[0]
+            value = ko_dict[1].split(', ')
+            item = [key, value]
+            code_dict.update([item])
+        print(code_dict)
+        return code_dict
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -15,12 +15,7 @@ class ParserTestCase(unittest.TestCase):
             self.parser.g.setup_document(html_bytes)
 
     def test_get_ko(self):
-        self.set_html('./pages/6.htm')
+        self.set_html('tests/pages/6.htm')
 
         ko_list = self.parser.get_ko_list()
-        self.assertListEqual([
-            {
-                'name': '',
-                'ko': ['1', '1+']
-            }
-        ], ko_list)
+        self.assertEqual({'основные коды': ['1', '1+']}, ko_list)
