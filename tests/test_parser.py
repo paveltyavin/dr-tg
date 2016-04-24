0760614a1eb547fe1ba3756d558b7271638243b4
Pavel Tyavin <pavel@tyavin.name>
2016-04-24 09:34:13 +0300

    change html to grab

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,13 +1,26 @@
 import unittest
-from codecs import open
+import codecs
 
 from d_parser import Parser
 
 
 class ParserTestCase(unittest.TestCase):
+    def setUp(self):
+        self.parser = Parser()
+
+    def set_html(self, filename):
+        with codecs.open(filename, encoding='cp1251') as f:
+            html = f.read()
+            html_bytes = bytes(html, encoding='cp1251')
+            self.parser.g.setup_document(html_bytes)
+
     def test_get_ko(self):
-        parser = Parser()
-        with open('./pages/6.htm', encoding='cp1251') as f:
-            parser.html = f.read()
-        ko_list = parser.get_ko_list()
-        self.assertEqual(['1', '1+'], ko_list)
+        self.set_html('./pages/6.htm')
+
+        ko_list = self.parser.get_ko_list()
+        self.assertListEqual([
+            {
+                'name': '',
+                'ko': ['1', '1+']
+            }
+        ], ko_list)
