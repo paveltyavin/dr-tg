9f7ab2d11c2c00df0dc6657a7e2561b9d7375725
Pavel Tyavin <pavel@tyavin.name>
2016-04-24 22:54:14 +0300

    парсинг взятости кодов. telepot в requirements drive_url на основе url параметров

--- d_parser.py
+++ d_parser.py
@@ -1,5 +1,19 @@
+from urllib.parse import urlencode
+
 from grab.base import Grab
 import re
+
+start_url = 'http://classic.dzzzr.ru/moscow/'
+drive_url = 'http://classic.dzzzr.ru/moscow/go/?{}'.format(urlencode({
+    'nostat': 'on',
+    'notext': '',
+    'notags': '',
+    'refresh': '30',
+    'log': '',
+    'legend': '',
+    'bonus': 'on',
+    'kladMap': '',
+}))
 
 
 class Parser(object):
@@ -9,10 +23,16 @@ class Parser(object):
     def __init__(self):
         self.g = Grab()
 
+    def auth(self, login='', password=''):
+        """Авторизация на сайте дозора"""
+        self.g.go(start_url)
+        self.g.doc.set_input('login', login)
+        self.g.doc.set_input('password', password)
+        self.g.doc.submit()
+
     def fetch(self):
-        """
-        Идет на сайт дозора, парсит страницу и обновляет текущее состояние
-        """
+        """Идет на сайт дозора, парсит страницу и обновляет текущее состояние"""
+        self.g.go(drive_url)
         self.state = self.get_state()
 
     def get_state(self):
@@ -34,19 +54,22 @@ class Parser(object):
 
     def get_sector_list(self):
         div_list = self.g.doc.select('//div[@class="zad"]')
-        div_html = next((div.html() for div in div_list if '<strong>Коды сложности</strong>' in div.html()), None)
+        div_html = next((div.html() for div in div_list if '<strong>Коды сложности</strong><br> ' in div.html()), None)
         if div_html is None:
             return []
         ko_part = div_html.split('<strong>Коды сложности</strong><br> ')[1]
-        ko_part = ko_part.replace('null', 'N').replace('<br></div>', '').replace('\n', '').replace('\r', '')
+        ko_part = ko_part.split('<br></div>')[0]
+        ko_part = ko_part.replace('null', 'N')
         result = []
-        for sector in ko_part.split('<br>'):
+        for sector in ko_part.split('<br> '):
             sector_name, sector_code_str = sector.split(': ')
             code_list = []
             for item in sector_code_str.split(', '):
+                taken = bool(self.red_span_re.match(item))
+                ko = self.red_span_re.findall(item)[0] if taken else item
                 code_list.append({
-                    'ko': item,
-                    'taken': False,
+                    'ko': ko,
+                    'taken': taken,
                 })
             result.append({
                 'name': sector_name,
@@ -56,8 +79,8 @@ class Parser(object):
 
     def get_tips_list(self):
         div_list = self.g.doc.select('//div[@class="title"]')
-        div_html = next((div for div in div_list if 'Подсказка l:' in div.html()), None)
-        if div_html is None:
+        div = next((div for div in div_list if 'Подсказка l:' in div.html()), None)
+        if div is None:
             return []
-        tip_text = div_html.node().getnext().text_content()
+        tip_text = div.node().getnext().text_content()
         return [tip_text]
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -16,23 +16,45 @@ class ParserTestCase(unittest.TestCase):
             html_bytes = bytes(html, encoding='cp1251')
             self.parser.g.setup_document(html_bytes)
 
-    def test_get_sector_list(self):
-        self.set_html('pages/1.html')
-
+    def test_get_sector_list_multiple(self):
+        """Несколько секторов"""
+        self.set_html('pages/10.html')
         sector_list = self.parser.get_sector_list()
-        self.assertListEqual([{
-            'name': 'основные коды',
-            'code_list': [{
-                'ko': '1',
-                'taken': False
-            }, {
-                'ko': '1+',
-                'taken': False
-            }],
-        }], sector_list)
+        self.assertListEqual([
+            {'name': 'бонусные коды', 'code_list': [{'ko': 'N', 'taken': False}]},
+            {'name': 'основные коды', 'code_list': [
+                {'ko': 'N', 'taken': False},
+                {'ko': 'N', 'taken': False},
+                {'ko': 'N', 'taken': False},
+                {'ko': 'N', 'taken': False},
+            ]},
+        ], sector_list)
+
+    def test_get_sector_list_one(self):
+        """Один сектор, многие коды взяты"""
+        self.set_html('pages/18.html')
+        sector_list = self.parser.get_sector_list()
+        self.assertListEqual([
+            {'name': 'основные коды', 'code_list': [
+                {'ko': '3', 'taken': True},
+                {'ko': '2', 'taken': False},
+                {'ko': '3', 'taken': True},
+                {'ko': '2', 'taken': False},
+                {'ko': '2', 'taken': True},
+                {'ko': '1', 'taken': False},
+                {'ko': '1', 'taken': True},
+                {'ko': '2', 'taken': False},
+                {'ko': '1', 'taken': True},
+                {'ko': '1+', 'taken': False},
+                {'ko': '1+', 'taken': False},
+                {'ko': '1', 'taken': False},
+                {'ko': '1+', 'taken': False},
+                {'ko': '1', 'taken': True},
+            ]},
+        ], sector_list)
 
     def test_get_tips_list(self):
-        self.set_html('pages/stub/18tip1.html')
+        self.set_html('pages/tip1.html')
 
         tips_list = self.parser.get_tips_list()
         self.assertListEqual(['Ответ на спойлер: пустырь'], tips_list)
